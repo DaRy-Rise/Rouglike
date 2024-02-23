74480684bbdeb8729fb36d5949cf512302f7babd
DaRy-Rise <64958256+DaRy-Rise@users.noreply.github.com>
2024-02-23 15:03:44 +0300

    spawn ability

--- Assets/Scripts/Camp/Masters/TreeOfAbilityManager.cs
+++ Assets/Scripts/Camp/Masters/TreeOfAbilityManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TreeOfAbilityManager : MonoBehaviour
@@ -5,12 +6,34 @@ public class TreeOfAbilityManager : MonoBehaviour
     [SerializeField]
     private GameObject treeOfAbility, dialogBox;
     public static bool isTreeOpen;
-    //string pulsePath = "Prefab/Camp/Ability/Sword/pulse.prefab";
-    //string destroyedPath = "Prefab/Camp/Ability/Sword/destroyed.prefab";
+    private List<GameObject> abilitiesPrefabs = new List<GameObject>();
     [SerializeField]
-    Animator anim;
-    [SerializeField]
-    GameObject[] points;
+    private GameObject staticPulsePrefab, pulsePrefab, destroyedPrefab;
+
+    private Point[,] points = 
+    { 
+        {
+            new Point(-4.384f, 2.978f),
+            new Point(-1.712f, 2.978f),
+            new Point(0.9629997f, 2.978f),
+            new Point(3.635f, 2.978f),
+            new Point(6.307f, 2.978f)
+        },
+        {
+            new Point(-4.384f, 0.026f),
+            new Point(-1.712f, 0.026f),
+            new Point(0.9629997f, 0.026f),
+            new Point(3.635f, 0.026f),
+            new Point(6.307f, 0.026f)
+        },
+        {
+            new Point(-4.384f, -2.85f),
+            new Point(-1.712f, -2.85f),
+            new Point(0.9629997f, -2.85f),
+            new Point(3.635f, -2.85f),
+            new Point(6.307f, -2.85f)
+        }
+    };
 
     void Start()
     {
@@ -21,15 +44,43 @@ public class TreeOfAbilityManager : MonoBehaviour
         isTreeOpen = true;
         DialogSystem.isBoxOpen = false;
         treeOfAbility.SetActive(true);
+        DrawAbility();
     }
     public void CloseTree()
     {
         DialogSystem.isBoxOpen = true;
         isTreeOpen = false;
+        DestroyAbility();
         treeOfAbility.SetActive(false);
     }
     private void DrawAbility()
+    { 
+        if (GlobalStat.swordDash == 0)
+        {
+            abilitiesPrefabs.Add(Instantiate(pulsePrefab, treeOfAbility.transform.position + new Vector3(points[0, 0].X, points[0, GlobalStat.swordDash].Y), Quaternion.identity, treeOfAbility.transform));
+            abilitiesPrefabs.Add(Instantiate(destroyedPrefab, treeOfAbility.transform.position + new Vector3(points[0, 1].X, points[0, GlobalStat.swordDash + 1].Y) + treeOfAbility.transform.forward, Quaternion.identity, treeOfAbility.transform));
+        }
+        else
+        {
+            for (int i = 0; i < GlobalStat.swordDash; i++)
+            {
+                abilitiesPrefabs.Add(Instantiate(staticPulsePrefab, treeOfAbility.transform.position + new Vector3(points[0, i].X, points[0, GlobalStat.swordDash - 1].Y) + transform.forward, Quaternion.identity, treeOfAbility.transform));
+            }
+        }
+        if (GlobalStat.swordDash != 5)
+        {
+            abilitiesPrefabs.Add(Instantiate(pulsePrefab, treeOfAbility.transform.position + new Vector3(points[0, GlobalStat.swordDash].X, points[0, GlobalStat.swordDash].Y) + treeOfAbility.transform.forward, Quaternion.identity, treeOfAbility.transform));
+        }
+        if (GlobalStat.swordDash < 4)
+        {
+            abilitiesPrefabs.Add(Instantiate(destroyedPrefab, treeOfAbility.transform.position + new Vector3(points[0, GlobalStat.swordDash].X, points[0, GlobalStat.swordDash].Y) + treeOfAbility.transform.forward, Quaternion.identity, treeOfAbility.transform));
+        }
+    }
+    private void DestroyAbility()
     {
- 
+        foreach (var item in abilitiesPrefabs)
+        {
+            Destroy(item);
+        }
     }
 }
---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,10 @@
+public class Point
+{
+    public float X { get; set; }
+    public float Y { get; set; }
+    public Point(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+}
