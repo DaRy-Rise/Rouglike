8356f639cbb9ef9d492f03849a21c3f5c2506602
Deymoss <yarmolovichv@gmail.com>
2024-05-06 23:59:11 +0300

    added tree with worons

--- Assets/TreeWithWorons.cs
+++ Assets/TreeWithWorons.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class TreeWithWorons : MonoBehaviour
 {
+
+    public GameObject ravenPrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +21,11 @@ public class TreeWithWorons : MonoBehaviour
     public void onAnimationEnds()
     {
         GetComponent<Animator>().SetBool("Start", false);
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject raven = Instantiate(ravenPrefab, new Vector3(transform.position.x + UnityEngine.Random.Range(0, 2f), transform.position.y + UnityEngine.Random.Range(0, 2f)), Quaternion.identity);
+            raven.transform.parent = transform;
+        }
     }
     // Update is called once per frame
     void Update()
