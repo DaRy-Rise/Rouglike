17d515abca549f4421bfecf526ec54d2cf8db6a8
Ivan Yarmalovich <i.yarmalovich@complitech.org>
2024-05-10 00:44:34 +0300

    fixed attack animation

--- Assets/Scripts/Level/SellerRandomizer.cs
+++ Assets/Scripts/Level/SellerRandomizer.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,15 +6,26 @@ public class SellerRandomizer : MonoBehaviour
     [SerializeField]
     private List<GameObject> sellerSawnPoints;
     private GameObject marketPrefab;
+    private int ChanceOfSellerSpawn = 10;
 
     void Start()
     {
         marketPrefab = Resources.Load<GameObject>("Prefab/Props/Level1/seller/Market");
-        SpawnPrefabs();
+        CheckSellerSpawnChance();
+    }
+    protected void CheckSellerSpawnChance()
+    {
+        int i = Random.Range(1, 101);
+        
+        if (i <= ChanceOfSellerSpawn)
+        {
+            SpawnPrefabs();
+        }
     }
 
     private void SpawnPrefabs()
     {
+        
         int rand = Random.Range(0, sellerSawnPoints.Count);
         GameObject market = Instantiate(marketPrefab, sellerSawnPoints[rand].transform.position, Quaternion.identity);
         market.transform.parent = sellerSawnPoints[rand].transform;
